2aec877713d09afc1399f0446141f2c3bf4b0741
Alekskorg <akozevnikov635@gmail.com>
2025-09-12 20:00:04 +0300

    Create ffmpeg-core.worker.js

---
+++ frontend/public/ffmpeg/ffmpeg-core.worker.js
@@ -0,0 +1,5 @@
+// This is a binary file placeholder.
+// Download the actual file from:
+// https://unpkg.com/@ffmpeg/core-mt@0.12.6/dist/esm/ffmpeg-core.worker.js
+// Note: We use the multi-threaded version for better performance if available.
+// If you stick to single-thread, this file is not needed. The hook is written for the single-threaded version.
